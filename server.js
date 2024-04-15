27db2884c5db7eeeda9705a51a83b6e0500bca1e
jjang6251 <jjang6251@gmail.com>
2024-04-15 14:52:28 +0900

    docker test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.json({message:"hello jjang"});
+    res.json({message:"hello jjang choi"});
 })
 
 app.listen(8000, () => {
