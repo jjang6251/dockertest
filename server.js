08217bb8ea60864eec797b6423b94e80fe507ce7
jjang6251 <jjang6251@gmail.com>
2024-04-15 13:16:50 +0900

    first

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({message:"hello"});
+})
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000`);
+});
