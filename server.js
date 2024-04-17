741ac8e3b96aee5780d69458e2eb7eeeb22e4fdb
jjang6251 <jjang6251@gmail.com>
2024-04-17 15:53:15 +0900

    doctest 4/17

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.json({message:"hello jjang choi"});
+    res.json({message:"hello jjang choi fighting!!"});
 })
 
 app.listen(8000, () => {
