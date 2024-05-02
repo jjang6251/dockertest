5417d877f6b6cfebe83fa73d04186fe32f85007c
jjang6251 <jjang6251@gmail.com>
2024-05-02 13:15:27 +0900

    post 메서드 추가

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.json({message:"hello codecure!"});
 })
 
+app.post('/posttest', (req, res) => {
+    const name = req.body.name;
+    return res.json({message:`${name}님 반갑습니다!`});
+})
+
 app.listen(8000, () => {
     console.log(`Server is running on port 8000`);
 });
